97cc3b085551e9530d6d61e714a268820ad02506
Stefan Birsan <83671730+StefanBirsan@users.noreply.github.com>
2023-12-19 19:22:37 +0200

    Add files via upload

---
+++ DAY15/day15_part1.py
@@ -0,0 +1,17 @@
+def main() -> None:
+    data = open("DAY15\input.txt").read().strip()
+
+    t = 0
+    for step in data.split(","):
+        v = 0
+        for ch in step:
+            v += ord(ch)
+            v *= 17
+            v %= 256
+
+        t += v
+
+    print(t)
+
+if __name__ == "__main__":
+    main()
